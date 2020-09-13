d0012d076f6747a235a744e0e8d5c6b7291d337e
bywdf <wdf1226@outlook.com>
2020-09-13 21:22:00 +0800

    chapter13-2

---
+++ chapter1-6/3_1.py
@@ -0,0 +1,6 @@
+names = ["zhangsan", "lisi", "wangwu"]
+names.append("Lilei")
+names.insert(0, "lucy")
+names.sort()
+print(names)
+print(sorted(names, reverse=True))
---
+++ chapter1-6/4_3.py
@@ -0,0 +1,5 @@
+s = list(range(2, 10, 2))
+print(s)
+
+squares = [i**2 for i in range(1, 11, 2)]
+print(squares)
