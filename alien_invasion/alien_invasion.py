e961703272df4a92cb09ce18db4cfac159c8357d
bywdf <wdf1226@outlook.com>
2020-09-16 22:02:18 +0800

    14.3.2

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -8,6 +8,7 @@ from alien import Alien
 import game_functions as gf
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 def run_game():
@@ -19,9 +20,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,
         ai_settings.screen_height))
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建存储游戏统计信息的实例,并创建记分牌
     stats = GameStats(ai_settings)
-    
+    sb = Scoreboard(ai_settings, screen, stats)
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
     # 创建一个用于储存子弹的编组,创建一个外星人组
@@ -39,6 +40,6 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
