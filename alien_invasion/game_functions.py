5b5e4be83abf2c775f838d2f2cd6807731aff534
bywdf <wdf1226@outlook.com>
2020-09-17 09:57:07 +0800

    14.3.5

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -97,6 +97,9 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
+        for aliens in collisions.values():
+            stats.score +=  ai_settings.alien_points * len(aliens)
+            sb.prep_score()
         stats.score += ai_settings.alien_points
         sb.prep_score()
 
